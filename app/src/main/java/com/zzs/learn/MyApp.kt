9129d5f6383a7ce749a001c8cc58d124c10d2133
arron <liuweig2258@163.com>
2023-08-15 21:00:54 +0800

    methodChannel 的使用

--- app/src/main/java/com/zzs/learn/MyApp.kt
+++ app/src/main/java/com/zzs/learn/MyApp.kt
@@ -1,9 +1,11 @@
 package com.zzs.learn
 
 import android.app.Application
+import android.os.Build
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
 
 class MyApp : Application() {
 
@@ -18,5 +20,17 @@ class MyApp : Application() {
 //        flutterEngine.navigationChannel.setInitialRoute("/")
         flutterEngine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
         FlutterEngineCache.getInstance().put(FLUTTER_ENGINE_ID,flutterEngine)
+        startFlutterService()
+    }
+
+    private fun startFlutterService() {
+        var channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger,"flutter_version")
+        channel.setMethodCallHandler{call,result ->
+            if(call.method.equals("getVersion")){
+                result.success(Build.BRAND)
+            }else {
+                result.notImplemented()
+            }
+        }
     }
 }
